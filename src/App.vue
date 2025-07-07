015f0727184ffcbbe1a2e3b53aed6e87cdf3c894
adz7180 <adamhaymour@gmail.com>
2025-07-07 14:24:34 -0600

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import './styles/global.scss';
+@import './styles/buttons.scss';
+@import './styles/layout.scss';
+
+#app {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
